2d333294fbedc395efc117cfd0f5972a2801e1a1
Prodromos Polychroniadis <propol_96@hotmail.com>
2019-05-21 17:40:09 +0300

    Update Gaussian_Algorithm.py

--- SVP_CVP/Gaussian_Algorithm.py
+++ SVP_CVP/Gaussian_Algorithm.py
@@ -7,14 +7,14 @@ print("(Gauss - Lagrange) Initializing algorithm to solve the SVP for L...")
 print("")
 
 B1 = np.linalg.norm(b1)**2
-m = (b1 * b2) / B1
+m = (b1[0]*b2[0] + b1[1]*b2[1]) / B1
 b2 = b2 - np.multiply(np.round(m), b1)
 B2 = np.linalg.norm(b2)**2
 
 while np.linalg.norm(b2) < np.linalg.norm(b1):
     b1, b2 = b2, b1
     B1 = B2
-    m = (b1 * b2) / B1
+    m = (b1[0]*b2[0] + b1[1]*b2[1]) / B1
     b2 = b2 - np.multiply(np.round(m), b1)
     B2 = np.linalg.norm(b2) ** 2
 
