e0fc07524fc7261c8ebdbe743bf2ffb82dd13d28
Prodromos Polychroniadis <propol_96@hotmail.com>
2019-05-21 16:45:52 +0300

    Add files via upload

---
+++ SVP_CVP/Gaussian_Algorithm.py
@@ -0,0 +1,22 @@
+import numpy as np
+import sympy as sym
+
+b1 = np.array([2, 1])
+b2 = np.array([3, 2])
+
+print("(Gauss - Lagrange) Initializing algorithm to solve the SVP for L...")
+print("")
+
+B1 = np.linalg.norm(b1)**2
+m = (b1 * b2) / B1
+b2 = b2 - np.multiply(np.round(m), b1)
+B2 = np.linalg.norm(b2)**2
+
+while np.linalg.norm(b2) < np.linalg.norm(b1):
+    b1, b2 = b2, b1
+    B1 = B2
+    m = (b1 * b2) / B1
+    b2 = b2 - np.multiply(np.round(m), b1)
+    B2 = np.linalg.norm(b2) ** 2
+
+print("b1= ", b1, "b2= ", b2)
