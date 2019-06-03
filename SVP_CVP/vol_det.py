4351466add3d3f5bb7df0c14fd4826ed54f297b8
Prodromos Polychroniadis <propol_96@hotmail.com>
2019-06-03 15:46:37 +0300

    Update vol_det.py

--- SVP_CVP/vol_det.py
+++ SVP_CVP/vol_det.py
@@ -1,3 +1,5 @@
+#Miscellaneous script
+#Check whether the determinant of an open base of a lattice is equal to its orignial base
 import numpy as np
 import GA
 
@@ -8,5 +10,5 @@ GA.ga(open_base)
 
 if np.linalg.det(original_base) == np.linalg.det(open_base):
     print("True")
-
-
+else:
+    print("False")
