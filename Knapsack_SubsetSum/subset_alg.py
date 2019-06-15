0fa977f5d1548b67bd3b0dff07328a12a6819c18
Prodromos Polychroniadis <propol_96@hotmail.com>
2019-06-15 10:47:44 +0300

    Update subset_alg.py

--- Knapsack_SubsetSum/subset_alg.py
+++ Knapsack_SubsetSum/subset_alg.py
@@ -1,26 +1,26 @@
 def subset_alg(x_list, target):
-    memo = dict()
-    result, _ = g(x_list, x_list, target, memo)
+    ndict = dict()
+    result, _ = g(x_list, x_list, target, ndict)
     return (sum(result), result)
 
 
-def g(v, w, S, memo):
+def g(v, w, S, ndict):
     subset = []
     id_subset = []
     for i, (x, y) in enumerate(zip(v, w)):
-        # Check if there is still a solution if we include v[i]
-        if f(v, i + 1, S - x, memo) > 0:
+        
+        if f(v, i + 1, S - x, ndict) > 0:
             subset.append(x)
             id_subset.append(y)
             S -= x
     return subset, id_subset
 
 
-def f(v, i, S, memo):
+def f(v, i, S, ndict):
     if i >= len(v):
         return 1 if S == 0 else 0
-    if (i, S) not in memo:    # <-- Check if value has not been calculated.
-        count = f(v, i + 1, S, memo)
-        count += f(v, i + 1, S - v[i], memo)
-        memo[(i, S)] = count  # <-- Memoize calculated result.
-    return memo[(i, S)]       # <-- Return memoized value.
+    if (i, S) not in ndict:    
+        count = f(v, i + 1, S, ndict)
+        count += f(v, i + 1, S - v[i], ndict)
+        ndict[(i, S)] = count  
+    return ndict[(i, S)]       
