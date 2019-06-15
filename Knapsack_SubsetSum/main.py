b535c9e141d8f8a462cfd05b795210720ad4a60c
Prodromos Polychroniadis <propol_96@hotmail.com>
2019-06-15 10:45:30 +0300

    Add files via upload

--- Knapsack_SubsetSum/main.py
+++ Knapsack_SubsetSum/main.py
@@ -1,1 +1,16 @@
+from time import time
+import subset_alg
 
+def main():
+    x_list = [10, 2, 4, 3, 1]
+    target= 5
+
+    time_start = time()
+    ss = subset_alg.subset_alg(x_list, target)
+    time_end = time()
+    
+    print('Subset found:', ss, time_end - time_start)
+    
+
+if __name__ == '__main__':
+    main()
